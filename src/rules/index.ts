3b9e9cfcf173da614ae9eeed2f2073c38409c007
Alexander T <alexander.tarasyuk@outlook.com>
2019-06-06 16:10:25 +0300

    export only-arrow-functions

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,7 @@
 import objectLiteralSurroundingSpace from './object-literal-surrounding-space';
 import noTypeAssertionWhitespace from './no-type-assertion-whitespace';
 import typeOperatorSpacing from './type-operator-spacing';
+import onlyArrowFunctions from './only-arrow-functions';
 import booleanTrivia from './boolean-trivia';
 import noDoubleSpace from './no-double-space';
 import noInOperator from './no-in-operator';
@@ -10,6 +11,7 @@ export default {
   'object-literal-surrounding-space': objectLiteralSurroundingSpace,
   'no-type-assertion-whitespace': noTypeAssertionWhitespace,
   'type-operator-spacing': typeOperatorSpacing,
+  'only-arrow-functions': onlyArrowFunctions,
   'no-double-space': noDoubleSpace,
   'boolean-trivia': booleanTrivia,
   'no-in-operator': noInOperator,
